d652f1a4b0f7b01d903a03c43d6cc6b8086fa26d
Adam Helinski <helinski.adam@outlook.com>
2018-03-02 09:11:30 +0100

    Rename project and modify api

--- README.md
+++ README.md
@@ -1,38 +1,56 @@
 # ADC via I2C
 
-[![Clojars Project](https://img.shields.io/clojars/v/dvlopt/horter-i2hae.svg)](https://clojars.org/dvlopt/horter-i2hae)
+[![Clojars
+Project](https://img.shields.io/clojars/v/dvlopt/i2c.horter-i2hae.svg)](https://clojars.org/dvlopt/i2c.horter-i2hae)
 
-Interact with [this](https://www.horter-shop.de/en/i2c-din-rail-modules/172-kit-i2c-analog-input-module-5-channel-10-bit-4260404260745.html)
-for reading up to 5 analog 10 bits inputs via I2C.
+Interact with
+[this](https://www.horter-shop.de/en/i2c-din-rail-modules/172-kit-i2c-analog-input-module-5-channel-10-bit-4260404260745.html)
+for reading up to 5 analog 10 bits inputs via
+[I2C](https://en.wikipedia.org/wiki/I%C2%B2C).
 
-Relies on [Icare](https://github.com/dvlopt/icare), an I2C clojure lib.
 
 ## Usage
 
+Using [dvlopt.i2c](https://github.com/dvlopt/i2c) for opening an I2C bus and
+reading the ADC :
+
 ```clj
-(require '[icare.core       :as i2c]
-         '[horter-i2hae.i2c :as adc])
+(require '[dvlopt.i2c                  :as i2c]
+         '[dvlopt.i2c.horter-i2hae.i2c :as adc])
 
 
-;; first, open an I2C bus where needed
+;; First, we need to open an I2C bus where needed
 (def bus
      (i2c/open "/dev/i2c-1"))
 
 
-;; then select the proper slave
-(i2c/select bus
-            0x08)
+;; Then, select our ADC
+(i2c/select-slave bus
+                  0x08)
 
 
-;; finally, get the raw data and then extract the value for each channel
-;; values are 10 bits unsigned integers, ie. between 0 and 1023 included
-(adc/process (adc/raw-data bus))
-;; {1 24, 2 546, 3 0, 4 245, 5 0}
+;; This is a byte array, our buffer for reading
+(def buff
+     (adc/data-buffer))
+
+
+;; Let's read (you should do some error checking)
+(i2c/read-bytes bus
+                buff)
+
+
+;; And finally, we can process the data and extract the values
+(adc/process buff)
+;; => {1 24
+;;     2 546
+;;     3 0
+;;     4 245
+;;     5 0}
 ```
 
 ## License
 
-Copyright © 2017 Adam Helinski
+Copyright © 2017-2018 Adam Helinski
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,21 +1,20 @@
-(defproject dvlopt/horter-i2hae
-            "0.0.0-alpha4"
+(defproject dvlopt/i2c.horter-i2hae
+            "0.0.0"
 
-  :description  "Clojure lib for reading analog inputs via I2C (Horter I2HAE)"
-  :url          "https://github.com/dvlopt/horter-i2hae"
-  :license      {:name "Eclipse Public License"
-                 :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dvlopt/icare "0.0.0-alpha0"]]
-  :profiles     {:dev {:source-paths ["dev"]
-                       :main         user
-                       :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
-                                      [org.clojure/spec.alpha "0.1.123"]
-                                      [org.clojure/test.check "0.9.0"]
-                                      [criterium              "0.4.4"]]
-                       :plugins      [[venantius/ultra "0.5.1"]
-                                      [lein-midje      "3.0.0"]
-                                      [lein-codox      "0.10.3"]]
-                       :codox        {:output-path  "doc/auto"
-                                      :source-paths ["src"]}
-                       :repl-options {:timeout 180000}
-                       :global-vars  {*warn-on-reflection* true}}})
+  :description "Clojure library for ADC via I2C (Horter I2HAE)"
+  :url         "https://github.com/dvlopt/i2c.horter-i2hae"
+  :license     {:name "Eclipse Public License"
+                :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles    {:dev {:source-paths ["dev"]
+                      :main         user
+                      :dependencies [[criterium              "0.4.4"]
+                                     [dvlopt/i2c             "0.0.0"]
+                                     [org.clojure/clojure    "1.9.0"]
+                                     [org.clojure/spec.alpha "0.1.123"]
+                                     [org.clojure/test.check "0.9.0"]]
+                      :plugins      [[lein-codox      "0.10.3"]
+                                     [venantius/ultra "0.5.2"]]
+                      :codox        {:output-path  "doc/auto"
+                                     :source-paths ["src"]}
+                      :repl-options {:timeout 180000}
+                      :global-vars  {*warn-on-reflection* true}}})
